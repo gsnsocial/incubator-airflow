6d14816ee6f0ebfa40ded192efbe74754b37bd8b
Dennis O'Brien <dennis@dennisobrien.net>
2016-11-28 16:08:11 -0800

    AIRFLOW-571 - address some of the code quality issues brought up by landscape.io

--- airflow/bin/cli.py
+++ airflow/bin/cli.py
@@ -745,7 +745,8 @@ def webserver(args):
     if ssl_cert and not ssl_key:
         raise AirflowException(
             'An SSL key must also be provided for use with ' + ssl_cert)
-    forwarded_allow_ips = args.forwarded_allow_ips or conf.get('webserver', 'forwarded_allow_ips')
+    forwarded_allow_ips = (args.forwarded_allow_ips or 
+                           conf.get('webserver', 'forwarded_allow_ips'))
 
     if args.debug:
         print(
@@ -793,8 +794,8 @@ def webserver(args):
         if ssl_cert:
             run_args += ['--certfile', ssl_cert, '--keyfile', ssl_key]
 
-        if args.forwarded_allow_ips:
-            run_args += ['--forwarded-allow-ips', args.forwarded_allow_ips]
+        if forwarded_allow_ips:
+            run_args += ['--forwarded-allow-ips', forwarded_allow_ips]
 
         run_args += ["airflow.www.app:cached_app()"]
 
